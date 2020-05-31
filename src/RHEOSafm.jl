4fa29cae54b904489117fad0a325fd6f9a348feb
Alessandra <bonfantiale.ab@gmail.com>
2020-05-31 19:00:33 +0100

    Added AFM_JPKimport.jl and AFM_preprocessing.jl

--- src/RHEOSafm.jl
+++ src/RHEOSafm.jl
@@ -1,5 +1,28 @@
+#!/usr/bin/env julia
+__precompile__(true)
+
 module RHEOSafm
 
-greet() = print("Hello World!")
+# installed from Julia package repository
+using RHEOS
+using NLopt
+
+# Base and stdlib imports
+import DelimitedFiles: readdlm, writedlm
+
+######################################################################
+
+# AFM_JPKimport.jl
+export importJPK
+
+# AFM_preprocessing.jl
+export contact_point
+
+######################################################################
+
+include("AFM_JPKimport.jl")
+include("AFM_preprocessing.jl")
+
+######################################################################
 
 end # module
