a85bf7380f13141a2d1ebdbb447ea429f87e2b8d
Alessandra <bonfantiale.ab@gmail.com>
2020-06-13 18:08:32 +0100

    add test hertz model

--- test/prova.jl
+++ test/prova.jl
@@ -3,6 +3,11 @@ using RHEOSafm
 using RHEOS
 using PyPlot
 
-input_file = joinpath(@__DIR__, "testdata", "AFM_contact_test.txt")
-data = importJPK(input_file, AFM(150e-9), sections = ["extend"]);
-data_contact = contact_point(data, AFM(150e-9), "hertz", (R = 150e-9,));
+    d = collect(0:0.1:10);
+    R = 1e-3
+    E = 1e2
+
+    F = 4/3 *E *R^0.5 .*d.^1.5;
+    d = collect(0:0.1:10.5)
+    F = vcat(zeros(5),F)
+    index = RHEOSafm.contact_hertz(F,d, (R = R,))
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,6 @@
 using RHEOSafm
 using RHEOS
 using Test
-using PyPlot
 
 include("importJPK.jl")
 include("preprocessing.jl")
