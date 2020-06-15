9616434802ece9aba730a3e3b6e0e5c863251dfe
Alessandra <bonfantiale.ab@gmail.com>
2020-06-15 19:14:40 +0100

    set up documentation

---
+++ docs/make.jl
@@ -0,0 +1,71 @@
+using Documenter
+using Literate
+using RHEOSafm
+
+"""
+    docprepare()
+Convert all source documentation (.jl and .md files)
+into markdown, ready for documentation build, and 
+relevant .jl files into Jupyter notebooks ready for
+deployment to notebooks branch.
+"""
+function docprepare()
+    # remove remnants of previous build and recreate staging dir
+    rm("docs/staging-docs", force=true, recursive=true)
+    mkdir("docs/staging-docs")
+
+    # copy readme to staging-docs
+    cp("README.md", "docs/staging-docs/index.md")
+
+    # copy assets to staging directory
+    cp("docs/src/assets", "docs/staging-docs/assets")
+
+    # iterate through src and convert/copy as appropriate
+    for file in readdir("docs/src")
+        if endswith(file, "md")
+            cp("docs/src/$file", "docs/staging-docs/$file")
+        elseif endswith(file, "jl")
+            Literate.markdown("docs/src/$file", "docs/staging-docs/"; documenter=true)
+        end
+    end
+end
+
+function notebookprepare()
+    # create notebook staging dir
+    mkdir("docs/staging-docs/notebooks")
+
+    # copy assets to notebooks staging directory
+    cp("docs/src/assets", "docs/staging-docs/notebooks/assets")
+
+    for file in readdir("docs/src")
+        if endswith(file, "jl")
+            Literate.notebook("docs/src/$file", "docs/staging-docs/notebooks/")
+        end
+    end
+end
+
+function maindocbuilder()
+    # prepare doc files from source
+    docprepare()
+
+    # prepare notebook files from source
+    notebookprepare()
+
+    # build docs from staging area
+    makedocs(modules=[RHEOSafm],
+            doctest= false, clean=true,
+            format = Documenter.HTML(),
+            sitename="RHEOSafm",
+            source = "staging-docs",
+            authors="Alessandra Bonfanti, Louis Kaplan, Alexandre Kabla",
+            pages = ["Home" => "index.md",
+                     "Example 01" => "example_01.md"])
+
+    deploydocs(repo = "github.com/JuliaRheology/RHEOSafm.jl.git",
+               deps = nothing,
+               make = nothing,
+               target = "build")
+
+end
+
+maindocbuilder()
---
+++ docs/src/example_01.jl
@@ -0,0 +1,14 @@
+# # Example 01
+
+# First import package
+
+using RHEOSafm
+using RHEOS
+
+# next import file
+input_file = joinpath(@__DIR__, "assets", "AFM_contact_test.txt")
+data = importJPK(input_file, AFM(150e-9), sections = ["extend"]);
+
+# and detect contact point
+data_contact = contact_point(data, AFM(150e-9), "hertz", (R = 150e-9,));
+length_contact = length(data_contact.t);
