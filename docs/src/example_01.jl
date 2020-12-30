4987464895b0d2f5fdf5e3c55f184e29d10a66df
Alessandra <bonfantiale.ab@gmail.com>
2020-12-30 13:45:19 +0000

    update example 01

--- docs/src/example_01.jl
+++ docs/src/example_01.jl
@@ -11,7 +11,8 @@ using Plots
 # Next, the output file provided by the JPK software is imported. The file contains the time-force-displacement values and they are automatically converted into time-stress-strain. 
 # The function [`importJPK`](@ref) requires the file path, the radious of the indenter, and the segments of the curve to upload (i.g. extend, pause, retract).
 input_file = joinpath(@__DIR__, "assets", "AFM_relaxation.txt")
-interface = AFM(150e-9);
+R = 150e-9; # Radious of the indenter
+interface = AFM(R);
 data = importJPK(input_file, interface, sections = ["extend", "pause"]);
 
 #md # !!! note "Beware!"
@@ -19,8 +20,22 @@ data = importJPK(input_file, interface, sections = ["extend", "pause"]);
 
 plot(data.t, data.σ, legend = false, xlabel = "Time", ylabel = "Stress")
 
-# Detect contact point
-data_contact = contact_point(data, AFM(150e-9), "hertz", (R = 150e-9,));
-length_contact = length(data_contact.t);
+# To detect the point at which approximately contact occurs in RHEOSafm it is possible to: 
+# 1) apply a force threshold
+# 2) apply Hertz spherical contact model
+# In this example the "threshold" method is used. An application of the Hertz method is availabel in example 02. 
+data_contact = contact_point(data, interface, "threshold", (threshold = 1e-8,));
+d_downsample = resample(data_contact, scale=1//2)
 
 plot(data_contact.t, data_contact.σ, legend = false, xlabel = "Time", ylabel = "Stress")
+
+SLS_model = modelstepfit(d_downsample, FractSLS_Zener, strain_imposed)
+
+SLS_predict = extract(data_contact, strain_only)
+# and calculate the stress based on the model
+SLS_predict = modelpredict(SLS_predict, SLS_model)
+# Now we can plot data and model together for comparison
+
+plot(data_contact.t, data_contact.σ, legend = false, xlabel = "Time", ylabel = "Stress")
+plot!(SLS_predict.t, SLS_predict.σ, legend = false, xlabel = "Time", ylabel = "Stress")
+
--- docs/src/example_02.jl
+++ docs/src/example_02.jl
@@ -2,13 +2,13 @@
 
 # First import package
 
-using RHEOSafm
-using RHEOS
+# using RHEOSafm
+# using RHEOS
 
 # next import file
-input_file = joinpath(@__DIR__, "assets", "AFM_contact_test.txt")
-data = importJPK(input_file, AFM(150e-9), sections = ["extend"]);
+# input_file = joinpath(@__DIR__, "assets", "AFM_contact_test.txt")
+# data = importJPK(input_file, AFM(150e-9), sections = ["extend"]);
 
 # and detect contact point
-data_contact = contact_point(data, AFM(150e-9), "hertz", (R = 150e-9,));
-length_contact = length(data_contact.t);
+# data_contact = contact_point(data, AFM(150e-9), "hertz", (R = 150e-9,));
+# length_contact = length(data_contact.t);
